ca4d42876db114783729afdbc2706cb1856df968
torrence payseno <tpayseno@gmail.com>
2020-02-05 08:05:59 -0500

    tweaks for heroku deployment

--- public/resources/js/app.js
+++ public/resources/js/app.js
@@ -11,7 +11,7 @@ weatherSearchForm.addEventListener('submit', (e) => {
             weatherreport.textContent = ''
 
             const location = search.value
-            fetch('http://localhost:3000/weather?address=' + encodeURIComponent(location)).then((response) => {
+            fetch('/weather?address=' + encodeURIComponent(location)).then((response) => {
                 response.json().then((data) => {
                     if (data.error) {
                         console.log(data.error)
