46de74db7d473206f969264f8bf2e8cea4369396
torrence payseno <tpayseno@gmail.com>
2020-02-05 08:27:53 -0500

    add missing brackets in client-side script

---
+++ .history/public/resources/js/app_20200205081242.js
@@ -0,0 +1,25 @@
+const weatherSearchForm = document.querySelector('form')
+const search = document.querySelector('input')
+const errmsg = document.querySelector('#wrpt-errmsg')
+const weatherreport = document.querySelector('#weather-rpt')
+
+
+
+weatherSearchForm.addEventListener('submit', (e) => {
+            e.preventDefault()
+            errmsg.textContent = 'Loading...'
+
+            const location = search.value
+            fetch('/weather?address=' + encodeURIComponent(location)).then((response) => {
+                response.json().then((data) =   > {
+                    if (data.error) {
+                        console.log(data.error)
+                        errmsg.textContent = data.error
+                    } else {
+                        errmsg.textContent = ''
+                        search.textContent = ''
+                        const report = 'It is currently ' + data.forecast + ' in ' + data.location + '. You searched for weather in ' + data.address + '.'
+                        weatherreport.textContent = report
+                    }
+                })
+            })
